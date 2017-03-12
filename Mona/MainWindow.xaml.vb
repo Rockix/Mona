5d3a2eb348e255b401b8c37b7cec59b4e52568d0
SAMMY <SAMMY@SLIHMHILS-PC>
2017-03-12 18:53:09 +0000

    Add project files.

---
+++ Mona/MainWindow.xaml.vb
@@ -0,0 +1,13 @@
+﻿Class MainWindow
+    Private Sub button_Click(sender As Object, e As RoutedEventArgs) Handles button.Click
+        Dim SAPI
+        SAPI = CreateObject("SAPI.spvoice")
+        SAPI.Speak(textBox.Text)
+
+
+
+
+
+
+    End Sub
+End Class
