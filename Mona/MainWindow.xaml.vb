7c888fd7f0b2889386b89b07b424a8687aad3440
rockix <sammyachman@gmail.com>
2017-03-12 19:56:30 +0000

    Fixed few bugs

--- Mona/MainWindow.xaml.vb
+++ Mona/MainWindow.xaml.vb
@@ -1,13 +1,18 @@
 ﻿Class MainWindow
+    Dim SAPI
     Private Sub button_Click(sender As Object, e As RoutedEventArgs) Handles button.Click
-        Dim SAPI
+
         SAPI = CreateObject("SAPI.spvoice")
         SAPI.Speak(textBox.Text)
+    End Sub
 
+    Private Sub textBox_KeyDown(sender As Object, e As KeyEventArgs) Handles textBox.KeyDown
+        If textBox.Text = "Enter text here..." Then
+            textBox.Clear()
+        End If
+    End Sub
 
-
-
-
-
+    Private Sub Window_Closing(sender As Object, e As ComponentModel.CancelEventArgs)
+        SAPI.Speak("Bye!")
     End Sub
 End Class
